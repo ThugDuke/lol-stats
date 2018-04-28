86899bde79a90061459612566806219b388cbf41
ThugDuke <m1212333@gmail.com>
2018-04-29 05:00:16 +0800

    debug for unranked player

--- lol_stats.py
+++ lol_stats.py
@@ -24,13 +24,17 @@ rank = data.select(porfile_tag)
 rank = rank[0].text.split()
 season = 3
 for i in range(0,len(rank),2) :
-    response = requests.get(stats_url+str(season)) 
-    season = season + 1
-    data = BeautifulSoup(response.text,'html.parser')
-    kda_tag = 'tr th h3'
-    record_tag = 'tbody tr td'
-    kda = data.select(kda_tag)
-    record = data.select(record_tag)
-    record = record[0].text.split()
-    kda = kda[0].text.split()    
-    print(rank[i], rank[i+1], kda[1], kda[2], record[0], record[1], record[2], record[3], record[4], record[5])
+    if rank[i+1] != '未參與積分':
+        response = requests.get(stats_url+str(season)) 
+        season = season + 1
+        data = BeautifulSoup(response.text,'html.parser')
+        kda_tag = 'tr th h3'
+        record_tag = 'tbody tr td'
+        kda = data.select(kda_tag)
+        record = data.select(record_tag)
+        record = record[0].text.split()
+        kda = kda[0].text.split()    
+        print(rank[i], rank[i+1], kda[1], kda[2], record[0], record[1], record[2], record[3], record[4], record[5])
+    else:
+        print(rank[i], rank[i+1])
+        season = season + 1
