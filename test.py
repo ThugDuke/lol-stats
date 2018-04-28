7792d71b37517e30bfa4125d5a521afe238f7167
ThugDuke <m1212333@gmail.com>
2018-04-29 03:56:09 +0800

    add stats 2

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-"""
+
 name = input('ID:')
 url = 'https://lol.moa.tw/summoner/show/'+name
 response = requests.get(url)
-"""
-url = 'https://lol.moa.tw/summoner/show/54978'
-response = requests.get(url)
+
 data = BeautifulSoup(response.text,'html.parser')
 porfile_tag = 'div.col-xs-6 dl.dl-horizontal.sub-jumbotron.h3'   
 stats_tag = 'ul.nav.nav-tabs li.dropdown ul.dropdown-menu li a'  
@@ -24,11 +22,15 @@ stats_url = 'https://lol.moa.tw' + ajax_url
 
 rank = data.select(porfile_tag)
 rank = rank[0].text.split()
+season = 3
 for i in range(0,len(rank),2) :
-    print(rank[i],rank[i+1])
-       
-response = requests.get(stats_url+'3')
-data = BeautifulSoup(response.text,'html.parser')
-tag = 'div.label.label-danger'
-kda = data.select(tag)
-print(kda[0].text)
+    response = requests.get(stats_url+str(season)) 
+    season = season + 1
+    data = BeautifulSoup(response.text,'html.parser')
+    kda_tag = 'tr th h3'
+    record_tag = 'tbody tr td'
+    kda = data.select(kda_tag)
+    record = data.select(record_tag)
+    record = record[0].text.split()
+    kda = kda[0].text.split()    
+    print(rank[i], rank[i+1], kda[1], kda[2], record[0], record[1], record[2], record[3], record[4], record[5])
