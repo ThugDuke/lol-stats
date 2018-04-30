6c23f4b364ad2addff008a0026cd042514f85b5e
ThugDuke <m1212333@gmail.com>
2018-04-30 16:36:28 +0800

    refactoring

--- lol_stats.py
+++ lol_stats.py
@@ -1,40 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_webdata(url,tag):
+    response = requests.get(url)
+    data = BeautifulSoup(response.text,'html.parser')
+    web_data = data.select(tag)    
+    return web_data
 
-name = input('ID:')
-url = 'https://lol.moa.tw/summoner/show/'+name
-response = requests.get(url)
+def get_ajax_url(url,tag):
+    stats = get_webdata(url,tag)
+    for stat in stats:   
+        if stat['data-url'].find('/Ajax/aggregate/') == 0:
+            ajax_url = stat['data-url']        
+            break
+    ajax_url = ajax_url[:-1]
+    return ajax_url
 
-data = BeautifulSoup(response.text,'html.parser')
-porfile_tag = 'div.col-xs-6 dl.dl-horizontal.sub-jumbotron.h3'   
-stats_tag = 'ul.nav.nav-tabs li.dropdown ul.dropdown-menu li a'  
+def main():    
+    profile_tag = 'div.col-xs-6 dl.dl-horizontal.sub-jumbotron.h3'   
+    stats_tag = 'ul.nav.nav-tabs li.dropdown ul.dropdown-menu li a'    
+    kda_tag = 'tr th h3'
+    record_tag = 'tbody tr td'
+    var = 1
+    while var == 1:
+        name = input('ID:')
+        if name == 0:
+            break        
+        profile_url = 'https://lol.moa.tw/summoner/show/'+name
+        stats_url = 'https://lol.moa.tw' + get_ajax_url(profile_url, stats_tag)
+        rank = get_webdata(profile_url,profile_tag)
+        rank = rank[0].text.split()
+        season = 3
+        for i in range(0,len(rank),2):
+            if rank[i+1] != '未參與積分':        
+                kda = get_webdata(stats_url + str(season),kda_tag)
+                record = get_webdata(stats_url + str(season),record_tag)                            
+                record = record[0].text.split()
+                kda = kda[0].text.split()
+                print(rank[i], rank[i+1], kda[1], kda[2], record[0],
+                    record[1], record[2], record[3], record[4], record[5])
+                season = season + 1
+            else:
+                print(rank[i], rank[i+1])
+                season = season + 1
 
-stats = data.select(stats_tag)
-for stat in stats:   
-    if stat['data-url'].find('/Ajax/aggregate/') == 0:
-        ajax_url = stat['data-url']        
-        break
-
-ajax_url = ajax_url[:-1]
-
-stats_url = 'https://lol.moa.tw' + ajax_url
-
-rank = data.select(porfile_tag)
-rank = rank[0].text.split()
-season = 3
-for i in range(0,len(rank),2) :
-    if rank[i+1] != '未參與積分':
-        response = requests.get(stats_url+str(season)) 
-        season = season + 1
-        data = BeautifulSoup(response.text,'html.parser')
-        kda_tag = 'tr th h3'
-        record_tag = 'tbody tr td'
-        kda = data.select(kda_tag)
-        record = data.select(record_tag)
-        record = record[0].text.split()
-        kda = kda[0].text.split()    
-        print(rank[i], rank[i+1], kda[1], kda[2], record[0], record[1], record[2], record[3], record[4], record[5])
-    else:
-        print(rank[i], rank[i+1])
-        season = season + 1
+if __name__ == '__main__' :
+    main()
