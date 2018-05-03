5d48aaaeccb8de03c6a5de28cf4edc9a04c557be
ThugDuke <m1212333@gmail.com>
2018-05-03 17:00:28 +0800

    add multiple search

--- lol_stats.py
+++ lol_stats.py
@@ -23,29 +23,32 @@ def main():
     record_tag = 'tbody tr td'
     var = 1
     while var == 1:
-        name = input('ID:')
-        if name == 'exit':
-            break        
-        profile_url = 'https://lol.moa.tw/summoner/show/'+name        
-        rank = get_webdata(profile_url,profile_tag)
-        if not rank:
-            print('查無此名字')
-            continue
-        rank = rank[0].text.split()        
-        stats_url = 'https://lol.moa.tw' + get_ajax_url(profile_url, stats_tag)
-        season = 3
-        for i in range(0,len(rank),2):
-            if rank[i+1] != '未參與積分':        
-                kda = get_webdata(stats_url + str(season),kda_tag)
-                record = get_webdata(stats_url + str(season),record_tag)                            
-                record = record[0].text.split()
-                kda = kda[0].text.split()
-                print(rank[i], rank[i+1], kda[1], kda[2], record[0],
-                    record[1], record[2], record[3], record[4], record[5])
-                season = season + 1
-            else:
-                print(rank[i], rank[i+1])
-                season = season + 1
+        names = input('ID:')
+        names = names.split()
+        if names == 'exit':
+                break
+        for name in names:
+            print('ID:'+name)
+            profile_url = 'https://lol.moa.tw/summoner/show/'+name        
+            rank = get_webdata(profile_url,profile_tag)
+            if not rank:
+                print('查無此名字')
+                continue
+            rank = rank[0].text.split()        
+            stats_url = 'https://lol.moa.tw' + get_ajax_url(profile_url, stats_tag)
+            season = 3
+            for i in range(0,len(rank),2):
+                if rank[i+1] != '未參與積分':        
+                    kda = get_webdata(stats_url + str(season),kda_tag)
+                    record = get_webdata(stats_url + str(season),record_tag)                            
+                    record = record[0].text.split()
+                    kda = kda[0].text.split()
+                    print(rank[i], rank[i+1], kda[1], kda[2], record[0],
+                        record[1], record[2], record[3], record[4], record[5])
+                    season = season + 1
+                else:
+                    print(rank[i], rank[i+1])
+                    season = season + 1
 
 if __name__ == '__main__' :
     main()
